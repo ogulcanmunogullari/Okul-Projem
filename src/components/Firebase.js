7747bd57b74950b3014b267f8071d6a0ddd0d3cf
Oğulcan Munoğulları <89843337+ogulcanmunogullari@users.noreply.github.com>
2022-05-24 19:39:12 +0300

    fazlalıklar silindi

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -3,14 +3,6 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyBVDrExMDoVdiv_gjhhCqb5c6PT8ohSJ5M",
-  // authDomain: "izledim-final.firebaseapp.com",
-  // databaseURL:
-  //   "https://izledim-final-default-rtdb.europe-west1.firebasedatabase.app",
-  // projectId: "izledim-final",
-  // storageBucket: "izledim-final.appspot.com",
-  // messagingSenderId: "930626182729",
-  // appId: "1:930626182729:web:007d76830e44bd9afda998",
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
